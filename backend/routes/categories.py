f46e0b9370e2097e023c8349f82a2ca3a5b42a10
Renan RodriguesDev <renanrodrigues7110@gmail.com>
2026-04-10 14:24:51 -0300

    feat: adicionar verificação de usuário nas rotas de categorias e lançar exceções apropriadas

--- backend/routes/categories.py
+++ backend/routes/categories.py
@@ -14,13 +14,27 @@ router = APIRouter(prefix="/categories", tags=["categories"])
 
 
 @router.get("/all", status_code=status.HTTP_200_OK, response_model=list[CategorySchema])
-async def get_all(session: Session = Depends(get_session)):
+async def get_all(
+    session: Session = Depends(get_session),
+    current_user: User = Depends(get_current_user),
+):
+    if not current_user:
+        raise exception_access_dained
     return session.query(Category).all()
 
 
 @router.get("/{id}", status_code=status.HTTP_200_OK, response_model=CategorySchema)
-async def get(id: int, session: Session = Depends(get_session)):
-    return session.query(Category).filter(Category.id == id).first()
+async def get(
+    id: int,
+    session: Session = Depends(get_session),
+    current_user: User = Depends(get_current_user),
+):
+    if not current_user:
+        raise exception_access_dained
+    category = session.query(Category).filter(Category.id == id).first()
+    if not category:
+        raise exception_category_not_found
+    return category
 
 
 @router.post(
